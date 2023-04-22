40d3ee67bf33b35808c8c60aa0d0675b59641502
allan de lima da silva <allan_11_lima@hotmail.com>
2023-04-22 17:14:57 -0300

    Varias edit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+// animação com scroll 
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry)=>{
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+// animação com scroll endS
+
+// pre loader 
+$(window).on("load",function(){
+  $(".loader-container").fadeOut(1000);
+  setTimeout(function(){
+    $(".Typewriter").addClass("animate");
+  },1000)
+})
+// pre loader end 
+
+
+
+
 function exibirTamanhoJanela() {
     // Obter a largura e altura da janela do navegador em pixels
     var largura = window.innerWidth;
